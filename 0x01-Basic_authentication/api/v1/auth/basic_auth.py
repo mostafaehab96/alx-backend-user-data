7eb34c677020c84968493d13b234f98d860b44e7
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-07 23:15:01 +0200

    Add task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,8 +11,19 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """Returns encoded header with base64"""
+        """Returns header part"""
         if (authorization_header is None or type(authorization_header) is not
                 str or not authorization_header.startswith("Basic ")):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string"""
+
+        try:
+            header = base64.b64decode(base64_authorization_header)
+            return header.decode('utf-8')
+        except Exception:
+            return None
