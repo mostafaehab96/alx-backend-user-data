63457fc78adac2dd80d5bc78ec13a819f02b37c4
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-08 00:21:33 +0200

    Add task 11 last mandatory

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,3 +58,12 @@ class BasicAuth(Auth):
                 return users[0]
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User')
+        """retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        base_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base_header)
+        email, password = self.extract_user_credentials(decoded_header)
+        user = self.user_object_from_credentials(email, password)
+        return user
