774314f823eada913d202064ef05968f0a4674d0
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-10 01:28:58 +0200

    Update before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,13 +53,15 @@ def before_request_fun():
     """
     if auth:
         excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         request.current_user = auth.current_user(request)
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+
 
 
 if __name__ == "__main__":
