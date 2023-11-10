39cd648d21fa7c82f8d8151756d85f104eabab8f
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-10 16:26:33 +0200

    Fix wrong password status

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -25,7 +25,7 @@ def login_user() -> str:
         return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"})
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
