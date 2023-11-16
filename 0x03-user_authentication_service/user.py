066a362e523c96ca1167fdb314f6a401f4381d2f
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-16 16:28:59 +0200

    Remove auto increment

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 class User(Base):
     """User class"""
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), unique=True, nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
