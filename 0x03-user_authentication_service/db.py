a733b55cc156a6c632c0cf4e3bd3c4c976bb2883
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-18 20:38:41 +0200

    Add update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,3 +51,13 @@ class DB:
                 return user
             else:
                 raise NoResultFound
+
+    def update_user(self, user_id: str, **kwargs) -> None:
+        """Update a user in the database"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
