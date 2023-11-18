39500ebd047926aadcf2a1436866c40145f8d699
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-18 20:48:14 +0200

    Add auth.py and hash_password function

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3
+
+"""Authentication Service"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password"""
+    password = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password, salt)
