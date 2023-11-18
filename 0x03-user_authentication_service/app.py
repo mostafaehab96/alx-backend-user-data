e566d3e6aaf372b13587c9db00c472d5dbd875e1
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-18 21:25:31 +0200

    Add create_user method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 """Basic Authentication Service"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """Index page"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', strict_slashes=False, methods=['POST'])
+def create_user():
+    """Creates a new user"""
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == '__main__':
